2f015465f6624bfd543a8d2d9d6a4d0cdcc76aab
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-09-12 20:36:51 -0400

    Practice creating views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:hello>')
+def print_string(hello):
+    print(hello)
+    return hello
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = [str(x) + '\n' for x in range(parameter)]
+    result = ''.join(numbers)
+    return result
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
